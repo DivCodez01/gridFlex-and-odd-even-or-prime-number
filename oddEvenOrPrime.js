2b619b1f38dd30404e7b93da21f0364f808c6ac2
divin-gakiza <divin.gakiza01@gmail.com>
2024-04-26 06:20:01 +0200

    secndcommit of adding odd,even or prime number

---
+++ oddEvenOrPrime.js
@@ -0,0 +1,19 @@
+const checkNumber = (function () {
+    return function (num) {
+        if (num < 0 || Number.isNaN(num) || typeof num == "undefined") {
+            return "Not allowed";
+        }
+
+        if (num % 2 == 0) {
+            return "This is Even number";
+        }
+
+        for (let i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return "This is Odd number";
+            }
+        }
+        return "This is Prime number"
+    }
+})();
+checkNumber()
